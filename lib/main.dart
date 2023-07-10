d10008b7311b7890dba9064702b525374d617cd8
AREEJ-QURAAN <areej.just.cpe@gmail.com>
2023-07-10 19:05:27 +0300

    add scroll

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,29 @@ const mapViewRoute = '/map_view/';
 
 ////ROUTES : -------------------
 ///
+///////Items TO DO LIST : -------------------
+List<Map<String, Function(dynamic)>> itemsToDo = [
+  {
+    'Profile': (context) {
+      Navigator.of(context).pushNamed(profileRoute);
+    },
+  },
+  {
+    'Item 2': (parameter) {
+      // Action for Item 2
+      print('Item 2 selected');
+    },
+  },
+  {
+    'Item 3': (parameter) {
+      // Action for Item 3
+      print('Item 3 selected');
+    },
+  },
+  // Add more items as needed
+];
+///////Items TO DO LIST : ---------------------------------------------------------
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
@@ -33,13 +56,30 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).pushNamed(profileRoute);
-        },
-        child: const Icon(Icons.accessibility),
-        backgroundColor: Colors.blueGrey,
+      body: SingleChildScrollView(
+        child: ListView.builder(
+          physics: const AlwaysScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: itemsToDo.length,
+          itemBuilder: (context, index) {
+            final item = itemsToDo[index].keys.first;
+            final action = itemsToDo[index].values.first;
+            return ListTile(
+              title: Text(item),
+              onTap: () {
+                action.call(context);
+              },
+            );
+          },
+        ),
       ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     Navigator.of(context).pushNamed(profileRoute);
+      //   },
+      //   child: const Icon(Icons.accessibility),
+      //   backgroundColor: Colors.blueGrey,
+      // ),
     );
   }
 }
